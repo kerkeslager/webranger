fd7b5b949dfb99d5650db42741da9faeb91f4a55
David Kerkeslager <david@kerkeslager.com>
2024-10-01 01:39:03 -0400

    Start work on the editor

--- editor/static/editor/editor.js
+++ editor/static/editor/editor.js
@@ -1,7 +1,42 @@
+import { Component } from 'preact';
 import { html } from 'htm/preact';
 
-export function Editor() {
-  return html`
-    <h1>Goodnight, moon!</h1>
-  `;
+const RANKS = 'AKQJT98765432';
+
+class Editor extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+    };
+  }
+
+  render() {
+    let rows = [];
+
+    for(var i = 0; i < RANKS.length; i++) {
+      let columns = [];
+
+      for(var j = 0; j < RANKS.length; j++) {
+        let rankI = RANKS.charAt(i);
+        let rankJ = RANKS.charAt(j);
+
+        if(i < j) {
+          columns.push(html`<td>${ rankI + rankJ + 's' }</td>`);
+        } else if(i > j) {
+          columns.push(html`<td>${ rankJ + rankI + 'o' }</td>`);
+        } else {
+          columns.push(html`<td>${ rankI + rankJ }</td>`);
+        }
+      }
+
+      rows.push(html`<tr>${ columns }</tr>`);
+    }
+
+    return html`
+      <table class='editor'>${ rows }</table>
+    `;
+  }
 }
+
+export { Editor };
