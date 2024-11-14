adcb896030c403816609055cfb52b8c1584f38e3
David Kerkeslager <david@kerkeslager.com>
2024-11-14 16:38:44 -0500

    Commit imports, generate combos based on hole name

--- editor/static/editor/editor.js
+++ editor/static/editor/editor.js
@@ -9,12 +9,47 @@ const SUITS = 'cdhs';
 class Hole extends Component {
   constructor(props) {
     super(props);
+
+    let name = this.props.name;
+    let combos = {};
+
+    if(this.isPair()) {
+      for(let i = 0; i < SUITS.length - 1; i++) {
+        for(let j = i + 1; j < SUITS.length; j++) {
+          combos[name[0] + SUITS[i] + name[1] + SUITS[j]] = 0;
+        }
+      }
+    } else if(this.isSuited()) {
+      for(let i = 0; i < SUITS.length; i++) {
+        combos[name[0] + SUITS[i] + name[1] + SUITS[i]] = 0;
+      }
+    } else {
+      for(let i = 0; i < SUITS.length; i++) {
+        for(let j = 0; j < SUITS.length; j++) {
+          if(i != j) {
+            combos[name[0] + SUITS[i] + name[1] + SUITS[j]] = 0;
+          }
+        }
+      }
+    }
+
+    console.log(combos);
+
     this.state = {
+      combos: combos,
     };
   }
 
   render() {
     return html`<div>${ this.props.name }</div>`;
+  }
+
+  isPair() {
+    return this.props.name.length === 2;
+  }
+
+  isSuited() {
+    return this.props.name.length === 3 && this.props.name[2] === 's';
   }
 }
 
