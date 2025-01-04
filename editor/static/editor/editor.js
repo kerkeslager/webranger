fdbf5dc70c78765b1ec645d7641becb6e639f4b2
David Kerkeslager <david@kerkeslager.com>
2025-01-04 14:25:37 -0500

    Start fleshing out Hole as a separate concept from Editor

--- editor/static/editor/editor.js
+++ editor/static/editor/editor.js
@@ -6,23 +6,33 @@ const CARDS = [...RANKS].map(rank => {
   return [...SUITS].map(suit => rank + suit);
 }).flat();
 
-function Editor(properties, children) {
-  return {
-    tag: 'table',
-    children: RANKS.split('').map((rank0, i0) => {
-      return {
-        tag: 'tr',
-        children: RANKS.split('').map((rank1, i1) => {
-          if(i0 < i1) {
-            return sml`<td>${ rank0 }${ rank1 }s</td>`;
-          } else if(i0 > i1) {
-            return sml`<td>${ rank1 }${ rank0 }o</td>`;
-          } else {
-            return sml`<td>${ rank0 }${ rank1 }</td>`;
-          }
-        }),
-      };
-    }),
+function Hole() {
+  return (properties, children) => {
+    return sml`<td>${ properties.descriptor }</td>`;
+  };
+}
+
+function Editor() {
+  return (properties, children) => {
+    let rows = [];
+
+    for(let i = 0; i < RANKS.length; i++) {
+      let columns = [];
+
+      for(let j = 0; j < RANKS.length; j++) {
+        if(i < j) {
+          columns.push(sml`<${ Hole() } descriptor=${ RANKS[i] + RANKS[j] + 's' } />`);
+        } else if(i > j) {
+          columns.push(sml`<${ Hole() } descriptor=${ RANKS[j] + RANKS[i] + 'o' } />`);
+        } else {
+          columns.push(sml`<${ Hole() } descriptor=${ RANKS[i] + RANKS[j] } />`);
+        }
+      }
+
+      rows.push(sml`<tr>${ columns }</tr>`);
+    }
+
+    return sml`<table>${ rows }</table>`;
   };
 }
 
