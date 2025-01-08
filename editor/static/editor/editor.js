b3e1e3d33fafb2769ee5302597c6fefb51503366
David Kerkeslager <david@kerkeslager.com>
2025-01-08 01:58:10 -0500

    Start implementing combo selection

--- editor/static/editor/editor.js
+++ editor/static/editor/editor.js
@@ -8,21 +8,58 @@ const CARDS = [...RANKS].map(rank => {
 
 function Hole(props, children) {
   let onClick = e => {
+    props.focusedHole.value = props.descriptor;
+  };
+
+  let onDblClick = e => {
     props.selected.value = !(props.selected.value);
   };
 
-  return (properties, children) => {
-    if(properties.selected) {
-      return sml`<td class=${ 'selected' } onClick=${onClick}>${ properties.descriptor }</td>`;
-    } else {
-      return sml`<td onClick=${onClick}>${ properties.descriptor }</td>`;
+  return (props, children) => {
+    let classes = [];
+    if(props.selected) classes.push('selected');
+    if(props.focusedHole === props.descriptor) classes.push('focused');
+    return sml`<td class=${ classes.join(' ') } onClick=${ onClick } onDblClick=${onDblClick}>${ props.descriptor }</td>`;
+  };
+}
+
+function ComboSelector(props, children) {
+  return (props, children) => {
+    let combos = [];
+
+    if(props.focusedHole.length === 2) {
+      // Pairs
+      for(let i = 0; i < SUITS.length - 1; i++) {
+        for(let j = i + 1; j < SUITS.length; j++) {
+          combos.push(props.focusedHole[0] + SUITS[i] + props.focusedHole[1] + SUITS[j]);
+        }
+      }
+    } else if(props.focusedHole[2] === 's') {
+      // Suited
+      for(let i = 0; i < SUITS.length; i++) {
+        combos.push(props.focusedHole[0] + SUITS[i] + props.focusedHole[1] + SUITS[i]);
+      }
+    } else if(props.focusedHole[2] === 'o') {
+      // Off suit
+      for(let i = 0; i < SUITS.length; i++) {
+        for(let j = 0; j < SUITS.length; j++) {
+          if(i != j) {
+            combos.push(props.focusedHole[0] + SUITS[i] + props.focusedHole[1] + SUITS[j]);
+          }
+        }
+      }
     }
+    return sml`<section class='combo-selector'>
+      <h1>${ props.focusedHole }</h1>
+      ${ combos.join(', ') }
+    </section>`;
   };
 }
 
 function Editor(props, children) {
   let descriptorMatrix = [];
   let range = {};
+  let focusedHole = new State('');
 
   for(let i = 0; i < RANKS.length; i++) {
     let columns = [];
@@ -49,9 +86,10 @@ function Editor(props, children) {
     return sml`<section class='editor'>
       <table>${
         descriptorMatrix.map(row => sml`<tr>${
-          row.map(column => sml`<${ Hole } descriptor=${ column } selected:=${ range[column] }/>`)
+          row.map(column => sml`<${ Hole } descriptor=${ column } focusedHole:=${ focusedHole } selected:=${ range[column] }/>`)
         }</tr>`)
       }</table>
+      <${ ComboSelector } focusedHole:=${ focusedHole } />
     </section>`;
   };
 }
